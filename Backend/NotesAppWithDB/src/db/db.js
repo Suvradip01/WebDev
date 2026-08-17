84530f63bc866143e8deb12bc9bb7ea5c66aaa97
Suvradip <ghoshsuvradip215@gmail.com>
2026-08-17 18:04:35 +0530

    Remove sensitive MongoDB connection string
    Updated MongoDB connection string to an empty string for security reasons.

--- Backend/NotesAppWithDB/src/db/db.js
+++ Backend/NotesAppWithDB/src/db/db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 async function connectDB() {
     try {
         console.log("Connecting to MongoDB...");
-        await mongoose.connect("mongodb://ghoshsuvradip215_db_user:X2Qr7Yb03Dm8djUZ@ac-9tbnijo-shard-00-00.dpwtrhs.mongodb.net:27017,ac-9tbnijo-shard-00-01.dpwtrhs.mongodb.net:27017,ac-9tbnijo-shard-00-02.dpwtrhs.mongodb.net:27017/mydb?ssl=true&replicaSet=atlas-fvuiwi-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0");
+        await mongoose.connect("");
         
         console.log("Database Connected Successfully");
     } catch (error) {
